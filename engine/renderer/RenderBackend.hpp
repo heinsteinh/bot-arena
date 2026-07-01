ec7d5f77dcb98f36837f41e176d361c8f3c93b8e
Heinstein F <heinsteinh@gmail.com>
2026-07-01 14:34:42 +0200

    feat(renderer): add abstract RenderBackend and Base aliases

---
+++ engine/renderer/RenderBackend.hpp
@@ -0,0 +1,28 @@
+#ifndef ENGINE_RENDERER_RENDERBACKEND_HPP
+#define ENGINE_RENDERER_RENDERBACKEND_HPP
+
+#include <glm/glm.hpp>
+#include <vector>
+
+#include "engine/core/Arena.hpp"
+#include "engine/core/Base.hpp"
+#include "engine/renderer/RenderQueue.hpp"
+
+namespace engine {
+
+class RenderBackend {
+ public:
+  virtual ~RenderBackend() = default;
+
+  virtual void beginFrame(int width, int height) = 0;
+  virtual void execute(const std::vector<RenderEntry>& entries,
+                       const glm::mat4& viewProjection, Arena& scratch) = 0;
+  virtual void endFrame() = 0;
+  virtual void readPixels(int x, int y, int width, int height, void* out) = 0;
+
+  static Scope<RenderBackend> Create();
+};
+
+}  // namespace engine
+
+#endif  // ENGINE_RENDERER_RENDERBACKEND_HPP
